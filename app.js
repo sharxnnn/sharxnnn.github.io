a0e0b5890966b4572d3cb61c622c8b267e099e9b
sharxnnn <sharonorji@unknown.home>
2024-09-15 23:08:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const images = document.querySelectorAll ("img"); 
 
 for (const image of images) {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://api.api-ninjas.com/v1/city")
     .then(response => response.json())
     .then(data => {
         image.src = data.message
